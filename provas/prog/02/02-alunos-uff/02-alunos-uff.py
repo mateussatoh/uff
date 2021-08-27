fc4abe423312544b08a3b9669dad469931efe30d
Mateus Satoh <mateussatoh@gmail.com>
2021-08-27 00:40:56 -0300

    prova: p2 de prog / sem usar lista

--- provas/prog/02/02-alunos-uff/02-alunos-uff.py
+++ provas/prog/02/02-alunos-uff/02-alunos-uff.py
@@ -1,32 +1,37 @@
 fileName = input()
 minMaxEfficiency = input().split()
 minMaxHours = input().split()
-def registration(line):
-  with open(f'{line}.txt', "r") as file:
-    lines = file.readlines()
-    efficiency = 0
-    totalHours = 0
-    validHours = 0
-    for index, line in enumerate(lines):
-      if index > 1:
-        data = line.strip("\n").split('#')
-        hours = int(data[-2:][0])
-        grades = float(data[-2:][1])
-        if(grades >= 6):
-          validHours += hours
-        efficiency += hours * grades
-        totalHours += hours
-      elif(index == 1):
-        name = line.strip('\n')
-      else:
-        course = line.strip('\n')
-  efficiencyCoef = efficiency / totalHours
-  file.close()
-  return [name, course, validHours, efficiencyCoef]
-with open(f'{fileName}.txt', "r") as file:
-  lines = file.readlines()
-  for line in lines:
-    studentData = registration(line.strip("\n"))
-    if(int(minMaxHours[0]) < studentData[2] < int(minMaxHours[1]) and float(minMaxEfficiency[0]) < studentData[3] < float(minMaxEfficiency[1])):
-        print(f'Nome:{studentData[0]} \nCurso:{studentData[1]} \nCarga Horária Efetiva: {studentData[2]} \nCoeficiente de Rendimento: {studentData[3]:.2f}\n')
+
+def registration(line, minMaxEfficiency, minMaxHours):
+    with open(f"{line}.txt", "r") as file:
+        efficiency = 0
+        totalHours = 0
+        validHours = 0
+        course = file.readline().strip("\n")
+        name = file.readline().strip("\n")
+        data = file.readline().strip("\n").split("#")
+        while data != [""]:
+            hours = int(data[-2:][0])
+            grades = float(data[-2:][1])
+            if grades >= 6:
+                validHours += hours
+            efficiency += hours * grades
+            totalHours += hours
+            data = file.readline().strip("\n").split("#")
+    efficiencyCoef = efficiency / totalHours
+    file.close()
+    if int(minMaxHours[0]) < validHours < int(minMaxHours[1]) and float(
+        minMaxEfficiency[0]
+    ) < efficiencyCoef < float(minMaxEfficiency[1]):
+        print(
+            f"Nome:{name} \nCurso:{course} \nCarga Horária Efetiva: {validHours} \nCoeficiente de Rendimento: {efficiencyCoef:.2f}\n"
+        )
+    return None
+
+
+with open(f"{fileName}.txt", "r") as file:
+    line = file.readline()
+    while line != "":
+        studentData = registration(line.strip("\n"), minMaxEfficiency, minMaxHours)
+        line = file.readline()
 file.close()
