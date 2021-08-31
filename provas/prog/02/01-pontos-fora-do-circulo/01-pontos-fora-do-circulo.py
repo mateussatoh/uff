cf247a911f7df5a782bea031d323c3ff9dd6b103
Mateus Satoh <mateussatoh@gmail.com>
2021-08-30 22:06:37 -0300

    fix: add P2Q1 prog / sem usar lista

--- provas/prog/02/01-pontos-fora-do-circulo/01-pontos-fora-do-circulo.py
+++ provas/prog/02/01-pontos-fora-do-circulo/01-pontos-fora-do-circulo.py
@@ -1,24 +1,36 @@
+import os
+
 fileName = input()
 circle = input().split()
-def isInsideCircle(line, circle,file):
-  lineX = int(line.split()[0])
-  lineY = int(line.split()[1])
-  circleX = int(circle[0])
-  circleY = int(circle[1])
-  radius = int(circle[2])    
-  dist = (((lineX - circleX)**2 + (lineY - circleY)**2))**(.5)
-  if (dist > radius):
-    file.write(line)
-    print(f'{lineX} {lineY}')
-  return None
-with open(f'{fileName}.txt', "r+") as file:
-  lines = file.readlines()
-  print(f'Pontos do Arquivo {fileName} antes das remoções:')
-  for line in lines:
-    print(line, end='')
-  file.seek(0)
-  print(f'\nPontos do Arquivo {fileName} depois das eventuais remoções:')
-  for line in lines:
-    isInsideCircle(line, circle, file)
-    file.truncate() 
+
+
+def isInsideCircle(line, circle, output):
+    lineX = int(line.split()[0])
+    lineY = int(line.split()[1])
+    circleX = int(circle[0])
+    circleY = int(circle[1])
+    radius = int(circle[2])
+    dist = (((lineX - circleX) ** 2 + (lineY - circleY) ** 2)) ** (0.5)
+    if dist > radius:
+        output.write(f"{lineX} {lineY}\n")
+    return None
+
+
+with open(f"{fileName}.txt", "r") as file:
+    with open("newfile.txt", "w") as output:
+        print(f"Pontos do Arquivo {fileName} antes das remoções:")
+        line = file.readline()
+        while line != "":
+            print(line.strip("\n"))
+            isInsideCircle(line, circle, output)
+            line = file.readline()
+        os.replace("newfile.txt", f"{fileName}.txt")
+file.close()
+
+with open(f"{fileName}.txt", "r") as file:
+    print(f"\nPontos do Arquivo {fileName} depois das eventuais remoções:")
+    line = file.readline()
+    while line != "":
+        print(line.strip("\n"))
+        line = file.readline()
 file.close()
