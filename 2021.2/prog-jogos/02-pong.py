6c68eb1397c8aed7e370e040b710976d8b3e04fe
mateussatoh <mateussatoh@gmail.com>
2021-11-17 19:05:11 -0300

    add: pong example

---
+++ 2021.2/prog-jogos/02-pong.py
@@ -0,0 +1,28 @@
+from PPlay.window import *
+from PPlay.gameimage import *
+from PPlay.sprite import *
+
+window = Window(640,480)
+window.set_title("Pong!")
+
+background = GameImage('./img/hobbit-town.jpg')
+
+ball = Sprite('./img/ring.png', 1)
+ball.x = background.width / 2 - ball.width / 2
+ball.y = background.height / 2 - ball.height / 2
+
+ballVelocityX = 0.3
+ballVelocityY = 0.3
+
+while True:
+    if (ball.x >= (background.width - ball.width) or ball.x <= 0):
+        ballVelocityX = ballVelocityX * -1
+    if (ball.y >= background.height or ball.y <= ball.height):
+        ballVelocityY = ballVelocityY * -1
+
+    ball.x = ball.x + ballVelocityX
+    ball.y = ball.y + ballVelocityY
+
+    background.draw()
+    ball.draw()
+    window.update()
